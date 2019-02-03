5ff9c217903e244cf319f42db564f58233122885
Sreeprakash Neelkantan <sree@schogini.com>
2019-02-03 11:23:46 +0530

    Final

--- Jenkinsfile-Docker-Deploy-Junit.groovy
+++ Jenkinsfile-Docker-Deploy-Junit.groovy
@@ -4,7 +4,8 @@ pipeline {
     parameters {
         string(name: 'MVNCACHE', defaultValue: '/Users/sree/vms/docker/jenkins/2019-feb-1/.m2', description: 'Maven repository cache in the host machine')    
         string(name: 'JENKINSDIR', defaultValue: '/Users/sree/vms/docker/jenkins/jenkins_home', description: 'The project path in the host machine')
-        
+        string(name: 'DOCKER_U', defaultValue: 'schogini', description: 'Docker Hib Username')
+        string(name: 'DOCKER_P', defaultValue: '', description: 'Docker Hub Password')        
     }
     
     stages {
@@ -41,6 +42,12 @@ pipeline {
           sh "sudo docker build -t schogini/tc:${BUILD_ID} tomcat"
         }
       }
+      stage('Push Docker Image') {
+        steps {
+          sh "sudo docker login -u=${params.DOCKER_U} -p=${params.DOCKER_P}"
+          sh "sudo docker push schogini/tc:${BUILD_ID}"
+        }
+      }
       stage('Deploy to Docker Image Tomcat') {
           steps {
             sh "sudo docker inspect my-tcc2 >/dev/null 2>&1 && sudo docker rm -f my-tcc2 || echo No container to remove. Proceed."
@@ -53,6 +60,12 @@ pipeline {
             sh "cp target/SampleJava1.war demo.war 2>&1 ; exit 0"
             sh "sudo docker run -d --name my-tcc -p 8123:8080 -v ${params.JENKINSDIR}/workspace/${JOB_BASE_NAME}/demo.war:/usr/local/tomcat/webapps/demo.war tomcat"
           }
+      }
+      stage('Deploy to Kubernetes') {
+        steps {
+           sh "sudo kubectl apply -f kubernetes/deploy-svc.yml"
+          #sh "sudo kubectl set image deploy/webapp-demo-deploy webapp=docker.io/schogini/docker.io/schogini/tc:${BUILD_ID}"
+        }
       } 
    }
 }
